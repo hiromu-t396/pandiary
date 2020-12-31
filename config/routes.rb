dda62c3bc4d2dbd820b96636c55641f08f227553
hiromu-t396 <hiromu1193966@gmail.com>
2020-12-31 21:38:18 +0900

    diarysルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'pages/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resource :diarys, only: [:index, :create]
 end
