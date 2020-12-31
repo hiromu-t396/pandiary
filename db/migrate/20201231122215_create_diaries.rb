199ea608f88f4b10f1e8f25008463486924235d9
hiromu-t396 <hiromu1193966@gmail.com>
2020-12-31 21:39:18 +0900

    diariesのマイグレーションファイル作成

---
+++ db/migrate/20201231122215_create_diaries.rb
@@ -0,0 +1,14 @@
+class CreateDiaries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :diaries do |t|
+      t.string :name_and_price
+      t.text :comment
+      t.integer :tag_list
+      t.float :float, null: false
+      t.references :user, foreign_key: true
+      # t.references :bakery_id, foreign_key: true
+      # t.references :bakery_name, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_171705) do
+ActiveRecord::Schema.define(version: 2020_12_31_122215) do
+
+  create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name_and_price"
+    t.text "comment"
+    t.integer "tag_list"
+    t.float "float", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_diaries_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_12_13_171705) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "diaries", "users"
 end
