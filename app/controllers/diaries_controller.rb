2d4578ec603356fc2522e540b3658b6ec2a7ab46
hiromu-t396 <hiromu1193966@gmail.com>
2021-01-10 11:09:07 +0900

    index,new,createアクションの記述

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,7 +1,25 @@
 class DiariesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
   end
 
+  def new
+    @diary = Diary.new
+  end
+
   def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:name_and_price, :comment, :tag_list, :rate, images: []).merge(user_id: current_user.id)
   end
 end
