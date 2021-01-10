df0e5e9b7be05e27e56b41fae0786d78cea2715a
hiromu-t396 <hiromu1193966@gmail.com>
2021-01-10 11:10:39 +0900

    newアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'diaries#index'
 
-  resources :diaries, only: [:index, :create]
+  resources :diaries, only: [:new, :index, :create]
 end
