d31580325f774d36344e24402ac8a92212a079d4
hiromu-t396 <hiromu1193966@gmail.com>
2021-01-12 17:11:49 +0900

    mondayモデルからopenモデルに名前変更

--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -1,4 +1,4 @@
 class Bakery < ApplicationRecord
   has_many :diaries
-  belongs_to :monday
+  belongs_to :open
 end
---
+++ app/models/open.rb
@@ -0,0 +1,28 @@
+class Open < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '4:00' },
+    { id: 3, name: '5:00' },
+    { id: 4, name: '6:00' },
+    { id: 5, name: '7:00' },
+    { id: 6, name: '8:00' },
+    { id: 7, name: '9:00' },
+    { id: 8, name: '10:00' },
+    { id: 9, name: '11:00' },
+    { id: 10, name: '12:00' },
+    { id: 11, name: '13:00' },
+    { id: 12, name: '14:00' },
+    { id: 13, name: '15:00' },
+    { id: 14, name: '16:00' },
+    { id: 15, name: '17:00' },
+    { id: 16, name: '18:00' },
+    { id: 17, name: '19:00' },
+    { id: 18, name: '20:00' },
+    { id: 19, name: '21:00' },
+    { id: 20, name: '22:00' },
+    { id: 21, name: '23:00' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :bakeries
+  end
