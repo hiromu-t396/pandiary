528531cd672d29c4113c9e85515396441144eedf
hiromu-t396 <hiromu1193966@gmail.com>
2021-01-26 21:13:47 +0900

    .mergeの記述追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,6 +1,6 @@
 class DiariesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+
   def index
   end
 
@@ -9,9 +9,9 @@ class DiariesController < ApplicationController
   end
 
   def create
+    binding.pry
     @diary_bakery = DiaryBakery.create(diary_params)
-     if @diary_bakery.save!
-      # @diary_bakery.save!
+     if @diary_bakery.save
        redirect_to action: :index
      else
       redirect_to action: :new
@@ -25,6 +25,6 @@ class DiariesController < ApplicationController
   end
 
   def diary_params
-    params.require(:diary_bakery).permit(:name_and_price, :comment, :tag_list, :rate, :name, :address, :tell, :monday_op_id, :monday_clo_id, :tuesday_op_id, :tuesday_clo_id, :wednesday_op_id, :wednesday_clo_id, :thursday_op_id, :thursday_clo_id, :friday_op_id, :friday_clo_id, :saturday_op_id, :saturday_clo_id, :sunday_op_id, :sunday_clo_id, :parking_id, :web_site, :instagram, :twitter, images: [])
+    params.require(:diary_bakery).permit(:name_and_price, :comment, :tag_list, :rate, :name, :address, :tell, :monday_op_id, :monday_clo_id, :tuesday_op_id, :tuesday_clo_id, :wednesday_op_id, :wednesday_clo_id, :thursday_op_id, :thursday_clo_id, :friday_op_id, :friday_clo_id, :saturday_op_id, :saturday_clo_id, :sunday_op_id, :sunday_clo_id, :parking_id, :web_site, :instagram, :twitter, images: []).merge(user_id: current_user.id)
   end
 end
